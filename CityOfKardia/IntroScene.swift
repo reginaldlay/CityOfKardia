69c43669253218f0c00b5e74cec4a9fa47159b04
reginaldlay <reginaldlay777@gmail.com>
2022-10-18 14:53:34 +0700

    tambah file doank

---
+++ CityOfKardia/IntroScene.swift
@@ -0,0 +1,13 @@
+//
+//  IntroScene.swift
+//  CityOfKardia
+//
+//  Created by Reginald Lay on 18/10/22.
+//
+
+import SpriteKit
+import GameplayKit
+
+class IntroScene: SKScene {
+    
+}
---
+++ CityOfKardia/PlayerStateController.swift
@@ -0,0 +1,29 @@
+//
+//  PlayerStateController.swift
+//  CityOfKardia
+//
+//  Created by Reginald Lay on 18/10/22.
+//
+
+import UIKit
+
+class PlayerStateController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
