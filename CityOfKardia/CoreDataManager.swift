b141fc52de0b3f87df0c5bceca42bf9d5f3de106
reginaldlay <reginaldlay777@gmail.com>
2022-10-27 17:47:25 +0700

    set core data Erry doank

---
+++ CityOfKardia/CoreDataManager.swift
@@ -0,0 +1,25 @@
+//
+//  CoreDataManager.swift
+//  CityOfKardia
+//
+//  Created by Reginald Lay on 27/10/22.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    
+    init () {
+        
+    }
+    
+    func readDataErry() {
+        //tbc
+    }
+    
+    func saveDataErry(erryLocation: String, erryXPos: Int, erryYPos: Int) {
+        //tbc
+    }
+    
+}
